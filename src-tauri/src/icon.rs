c163f63422a62ed7b3ed0fd37abb6f34275468ec
satler-git <mitsclatw24@mbr.nifty.com>
2024-08-04 07:28:54 +0000

    Use Rgba instead to u32 in find_most_used_color

--- src-tauri/src/icon.rs
+++ src-tauri/src/icon.rs
@@ -3,7 +3,13 @@ pub mod shuffle;
 pub mod win_icons;
 use crate::img;
 
+use image::{Pixel as _, Rgba};
 use windows::Win32::Foundation::HWND;
+
+fn cast_rgbau8_to_u32(rgba: &Rgba<u8>) -> u32 {
+    let rgb = rgba.to_rgb();
+    ((rgb[0] as u32) << 16) | ((rgb[1] as u32) << 8) | (rgb[2] as u32)
+}
 
 /// use komorebi_client::Window::hwnd for get hwnd
 impl From<HWND> for super::WindowForSend {
@@ -13,7 +19,7 @@ impl From<HWND> for super::WindowForSend {
             icon: super::Icon {
                 base64_icon: (img::convert_img_base64(&icon)),
             },
-            accent_color: img::find_most_used_color(&icon).unwrap(),
+            accent_color: cast_rgbau8_to_u32(&img::find_most_used_color(&icon).unwrap()),
         }
     }
 }
--- src-tauri/src/img.rs
+++ src-tauri/src/img.rs
@@ -1,6 +1,6 @@
 use anyhow::{bail, Context as _, Ok, Result};
 use fxhash::FxHashMap;
-use image::{DynamicImage, Pixel, Rgba};
+use image::{DynamicImage, Rgba};
 
 /// pngをbase64_pngに変換する
 /// `format!("data:image/png;base64,{}", resp_base64)`
@@ -15,31 +15,29 @@ pub fn convert_img_base64(image_from: &DynamicImage) -> String {
     base64::Engine::encode(&base64::engine::general_purpose::STANDARD, image_data)
 }
 
-fn cast_rgbau8_to_usize(rgba: &Rgba<u8>) -> usize {
-    let rgb = rgba.to_rgb();
-    ((rgb[0] as usize) << 16) | ((rgb[1] as usize) << 8) | (rgb[2] as usize)
-}
-
 /// 一番多く使われている色を`u32`で返す
 /// 計算量が多い(アイコンはそこまでサイズがデカくないから大丈夫かも)
-pub fn find_most_used_color(image: &DynamicImage) -> Result<u32> {
+pub fn find_most_used_color(image: &DynamicImage) -> Result<Rgba<u8>> {
     if let DynamicImage::ImageRgba8(img) = image {
         // 各ピクセルをイテレートして色情報を取得
         let pixels = img.pixels();
-        let mut counts: FxHashMap<usize, u32> = FxHashMap::default();
+        let mut counts: FxHashMap<&Rgba<u8>, u32> = FxHashMap::default();
 
         for pi in pixels {
-            let count = counts.entry(cast_rgbau8_to_usize(pi)).or_insert(0);
+            let count = counts.entry(pi).or_insert(0);
             *count += 1;
         }
 
         let most_common_color = counts
             .iter()
-            .filter(|&(&x, _)| x != (0 as usize))
+            .filter(|&(&x, _)| match x {
+                Rgba([_, _, _, 0]) => false,
+                _ => true
+            })
             .max_by_key(|i| i.1)
-            .context("Unable to find the most common color")?;
+            .context("Unable to find the most common color")?.0;
 
-        Ok(*most_common_color.0 as u32)
+        Ok(**most_common_color)
     } else {
         bail!("Unable to cast down to a Rgba image.")
     }
@@ -49,28 +47,27 @@ pub fn find_most_used_color(image: &DynamicImage) -> Result<u32> {
 mod tests {
     use super::*;
     use anyhow::{Ok, Result};
-    use image::{DynamicImage, Rgb};
+    use image::{DynamicImage, Rgba};
 
-    fn make_one_colored_image(rgb: &Rgb<u8>) -> Result<DynamicImage> {
+    fn make_one_colored_image(rgba: &Rgba<u8>) -> Result<DynamicImage> {
         let width = 54;
         let height = 54;
 
-        let image: DynamicImage = image::ImageBuffer::from_pixel(width, height, *rgb).into();
+        let image: DynamicImage = image::ImageBuffer::from_pixel(width, height, *rgba).into();
 
         Ok(image.to_rgba8().into())
     }
 
-    fn check_one_colored_image(rgb: Rgb<u8>) -> Result<()> {
-        let image = &make_one_colored_image(&rgb)?;
-        let rgb_u32 = ((rgb[0] as u32) << 16) | ((rgb[1] as u32) << 8) | (rgb[2] as u32);
-        assert_eq!(find_most_used_color(image)?, rgb_u32);
+    fn check_one_colored_image(rgba: Rgba<u8>) -> Result<()> {
+        let image = &make_one_colored_image(&rgba)?;
+        assert_eq!(find_most_used_color(image)?, rgba);
         Ok(())
     }
 
     #[test]
     fn one_colored_images() -> Result<()> {
-        check_one_colored_image(Rgb([255, 255, 255]))?;
-        check_one_colored_image(Rgb([0, 0, 0]))?;
+        check_one_colored_image(Rgba([255, 255, 255, 255]))?;
+        check_one_colored_image(Rgba([10, 10, 10, 255]))?;
         Ok(())
     }
 }
